58681ff1f6e995643274bc1c117395924f38f9e4
SayanRit123 <110761507+SayanRit123@users.noreply.github.com>
2025-04-23 22:49:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ import time
 app = Flask(__name__)
 CORS(app)  # To allow frontend JS to access this API
 
+session = boto3.Session(
+    aws_access_key_id='AKIAS2RBT7PDOMXMH6OM',
+    aws_secret_access_key='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',
+    region_name='us-east-1'
+)
+
 # Initialize DynamoDB client
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')  # e.g. 'ap-south-1'
+dynamodb = session.resource('dynamodb', region_name='us-east-1')  # e.g. 'ap-south-1'
 table = dynamodb.Table('Machine_data')  # Replace with your actual table name
 
 @app.route('/', methods=['GET'])
